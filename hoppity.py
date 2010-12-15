a85a310d9d728ad63048251dbc627b4e2a4bfdec
Russell <miller.time.baby@gmail.com>
2010-12-14 20:16:44 -0800

    first puzzle done

---
+++ hoppity.py
@@ -0,0 +1,24 @@
+import sys
+
+def read_file():
+  if not sys.argv[1]:
+    sys.exit(1)
+  return open(sys.argv[1], 'rU').read()
+
+def do_output(number):
+  for i in range(1,number):
+    if i%3 == 0 and i%5 != 0:
+      print("Hoppity")
+    elif i%5 == 0 and i%3 != 0:
+      print("Hophop")
+    elif i%3 == 0 and i%5 == 0:
+      print("Hop")
+
+def main():
+  if len(sys.argv) < 2:
+    print("Please provide a filename.")
+  number_string = read_file()
+  do_output(int(number_string))
+
+if __name__ == '__main__':
+  main()
